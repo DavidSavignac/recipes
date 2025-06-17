d79f6f5ca3badc09fe6aa35919f9f81d44c764e1
DavidSavignac <david.savignac@gmail.com>
2025-06-17 18:03:35 +0200

    Add customized queries for recipes

--- src/main/java/com/gmail/davidsavignac/cooking/recipes/repository/RecipeRepository.java
+++ src/main/java/com/gmail/davidsavignac/cooking/recipes/repository/RecipeRepository.java
@@ -2,10 +2,18 @@ package com.gmail.davidsavignac.cooking.recipes.repository;
 
 import com.gmail.davidsavignac.cooking.recipes.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
     List<Recipe> findByRecipeTypeId(Long recipeTypeId);
+
+    @Query("SELECT rec FROM Recipe rec WHERE rec.name = ?1")
+    List<Recipe> findByNameCustom(String name);
+
+    @Query(value = "SELECT rec.* FROM Recipe rec WHERE rec.name = ?1", nativeQuery = true)
+    List<Recipe> findByNameNative(String name);
+
 }
--- src/test/java/com/gmail/davidsavignac/cooking/recipes/repository/RecipeRepositoryTest.java
+++ src/test/java/com/gmail/davidsavignac/cooking/recipes/repository/RecipeRepositoryTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
 class RecipeRepositoryTest {
@@ -18,9 +18,23 @@ class RecipeRepositoryTest {
     @Test
     void findByRecipeTypeId() {
 
-        
+
         List<Recipe> recipes = recipeRepository.findByRecipeTypeId(1L);
         assertFalse(recipes.isEmpty());
 
+    }
+
+    @Test
+    void findByNameCustom() {
+
+        List<Recipe> recipes = recipeRepository.findByNameCustom("Pasta alla saracena");
+        assertFalse(recipes.isEmpty());
+    }
+
+    @Test
+    void findByNameNative() {
+
+        List<Recipe> recipes = recipeRepository.findByNameNative("Pasta alla saracena");
+        assertFalse(recipes.isEmpty());
     }
 }
